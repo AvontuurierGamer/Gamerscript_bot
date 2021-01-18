9e4b6fd0d9ae0390c307d75648a3e377a7e1b579
AvontuurierGamer <76844822+AvontuurierGamer@users.noreply.github.com>
2021-01-18 08:54:27 +0100

    Update testhelp.js

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -24,7 +24,7 @@ module.exports.run = async(bot, message, args) => {
 
         if(emoji == "⏩") {
 
-            message.delete();
+            promptMessage.delete();
 
             message.channel.send(embedtest)
 
