8410b73017a6c0d50e598c5a4a838d016ae471f4
AvontuurierGamer <76844822+AvontuurierGamer@users.noreply.github.com>
2021-01-20 11:01:31 +0100

    init

--- commands/help.js
+++ commands/help.js
@@ -2,23 +2,123 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    message.delete();
+    var embedPrompt = new discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle("help")
+        .addField("overig = 😉")
+        .addField("mod = ⭐")
+        .addField("ticket = 🎫")
+        .addField("fun = 😁")
+        .addField("anuleren = ❌")
+        .setFooter("help")
+        .setTimestamp()
 
-    var botEmbed =  new discord.MessageEmbed()
+    var embedhelpoverig = new discord.MessageEmbed()
+        .setTitle("Help")
+        .setColor("#03fc0f")
+        .addFields(
+            {name: "!userinfo", value: "`zie speler informatie`"},
+            {name: "!suggest", value: "`schrijf een suggestie`"},
+            {name: "!review", value: "`schrijf een review`"},
+        )
+        .setFooter("Help")
+        .setTimestamp()
+
+    var embedhelpfun =  new discord.MessageEmbed()
+        .setTitle("help")
+        .setColor("#03fc0f")
+        .addFields(
+            {name:"sps", value: "`Speel steen papier schaar tegen de bot`"}
+        )
+        .setFooter("help")
+        .setTimestamp()
+
+        var embedhelpmod =  new discord.MessageEmbed()
             .setTitle("Help")
             .setColor("#03fc0f")
             .addFields(
-                {name: "😉overig", value: "`!help-overig`"},
-                {name:"⭐mod", value: "`!help-mod`"},
-                {name: "😁fun", value: "`!help-fun`"},
-                {name: "🎫ticket", value: "`!help-ticket`"}
+                {name:"embed", value: "`maak een embed in een bepaald kanaal`"},
+                {name: "kick", value: "`hiermee kan een staff lid iemand kicken`"},
+                {name: "ban", value: "`hiermee kan een staff lid iemand bannen`"},
+                {name: "clear", value: "`verwijder een aantal berichten`"}
             )
             .setFooter("Help")
             .setTimestamp()
 
+        var botEmbed =  new discord.MessageEmbed()
+            .setTitle("help")
+            .setColor("#03fc0f")
+            .addFields(
+                {name:"ticket", value: "`maak een ticket aan`"},
+                {name: "close", value: "`sluit een ticket dit kan alleen een stafflid`"},
+                {name: "add", value: "`voeg iemand toe aan een ticket`"},
+                {name: "remove", value: "`verwijder iemand uit een ticket`"}
+            )
+            .setFooter("help")
+            .setTimestamp()
 
-        return message.channel.send(botEmbed);
+    message.channel.send(embedPrompt).then(async msg => {
+
+        message.delete();
+
+        var emoji = await promptMessage(msg, message.author, 30, ["😉", "⭐", "🎫", "😁", "❌"])
+
+        if(emoji == "😉") {
+
+            msg.edit(embedhelpoverig).then((msg) =>  {
+                setTimeout(function(){
+                msg.edit(embedtest);
+                }, 1)
+            })
+
+        } else if(emoji == "⭐") {
+
+            msg.edit(embedhelpmod).then((msg) =>  {
+                setTimeout(function(){
+                msg.edit(embedtest);
+                }, 1)
+            })
+
+        } else if(emoji == "🎫") {
+
+            msg.edit(embedhelpticket).then((msg) =>  {
+                setTimeout(function(){
+                msg.edit(embedtest);
+                }, 1)
+            })
+
+        } else if(emoji == "😁") {
+
+            msg.edit(embedhelpfun).then((msg) =>  {
+                setTimeout(function(){
+                msg.edit(embedtest);
+                }, 1)
+            })
+
+        } else if(emoji == "❌") {
+            
+            message.delete();
+
+            message.send("geanuleerd").then(msg => msg.delete({timeout: 5000}));
+        }
+
+    });
+    
+}
+
+async function promptMessage(message, author, time, reactions) {
+
+    time *= 1000;
+
+    for (const reaction of reactions) {
+        await message.react(reaction);
     }
+
+    const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+    return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
+
+}
     
 
 module.exports.help = {
